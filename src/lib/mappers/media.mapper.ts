cd65adf4c4d384a755d5cc7f56cea1eea10c01b2
Erick Hernández Torres <satcoresolutions@gmail.com>
2026-07-30 16:49:26 -0500

    cloudinary
    plugin de cloudinary para visualizacion de imagnes

--- src/lib/mappers/media.mapper.ts
+++ src/lib/mappers/media.mapper.ts
@@ -17,21 +17,16 @@ export function mapMedia(
 
   }
 
-  console.log("================================");
-  console.log("mapMedia ejecutado");
-  console.log("STRAPI_URL:", STRAPI_URL);
-  console.log("URL original:", data.url);
-  console.log("URL final:", `${STRAPI_URL}${data.url}`);
-  console.log("================================");
-
   return {
 
     ...data,
 
     url:
-      data.url
-        ? `${STRAPI_URL}${data.url}`
-        : "",
+  !data.url
+    ? ""
+    : data.url.startsWith("http")
+      ? data.url
+      : `${STRAPI_URL}${data.url}`,
 
     formats:
       data.formats
